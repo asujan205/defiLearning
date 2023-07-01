73bd27056d374aa292a763936bc5334eb3cb9572
asujan205 <asujan205@gmail.com>
2023-07-01 11:34:35 +0545

    #SwapExactInputSingle

--- contracts/uniswapv3defi/singleSwap.sol
+++ contracts/uniswapv3defi/singleSwap.sol
@@ -14,7 +14,28 @@ contract SingleSwap {
     address public constant WETH9 = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
     address public constant USDC = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
 
+    uint24 public constant DAI_USDC_POOL_FEE = 3000;
+
     constructor() {
         uniswapV3Router = ISwapRouter(UNISWAP_V3_ROUTER);
+    }
+
+    function SwapExactInputSingle (unit256 amountIn) external returns (uint256 amountOut){
+        TransferHelper.safeApprove(DAI, UNISWAP_V3_ROUTER, amountIn);
+        TransferHelper.safeTransferFrom(Dai, msg.sender,address(this), amountIn);
+
+        ISwapRouter.ExactInputSingleParams memory params =
+            ISwapRouter.ExactInputSingleParams({
+                tokenIn: DAI,
+                tokenOut: WETH9,
+                fee: DAI_USDC_POOL_FEE,
+                recipient: msg.sender,
+                deadline: block.timestamp + 1800,
+                amountIn: amountIn,
+                amountOutMinimum: 0,
+                sqrtPriceLimitX96: 0
+            });
+
+        amountOut = uniswapV3Router.exactInputSingle(params);
     }
 }
