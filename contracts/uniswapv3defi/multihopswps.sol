c9c77021a08fc9cfc57651c2fde6c6e07d743674
asujan205 <asujan205@gmail.com>
2023-07-03 11:09:03 +0545

    #swaps

--- contracts/uniswapv3defi/multihopswps.sol
+++ contracts/uniswapv3defi/multihopswps.sol
@@ -14,5 +14,12 @@ contract SwapMultiHops{
             swapRouter = _swapRouter;
         }
 
-        
+          address public constant DAI = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
+    address public constant WETH9 = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+    address public constant USDC = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
+
+        uint24 public constant poolFee = 3000;
+
+
+
 }
