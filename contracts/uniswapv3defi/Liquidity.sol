124be298bef9d28637072e339d7739a62e99b106
asujan205 <asujan205@gmail.com>
2023-07-07 21:19:18 +0545

    ##liquidity

--- contracts/uniswapv3defi/Liquidity.sol
+++ contracts/uniswapv3defi/Liquidity.sol
@@ -24,5 +24,32 @@ contract LiquidityProvider is IERC721Receiver{
     }
 
     mapping(uint256 => Deposit) public deposits;
+       constructor(
+        INonfungiblePositionManager _nonfungiblePositionManager
+    ) {
+        nonfungiblePositionManager = _nonfungiblePositionManager;
+    }
+
+      function onERC721Received(
+        address operator,
+        address,
+        uint256 tokenId,
+        bytes calldata
+    ) external override returns (bytes4) {
+        // get position information
+
+        _createDeposit(operator, tokenId);
+
+        return this.onERC721Received.selector;
+    }
+
+    function _createDeposit(address owner, uint256 tokenId) internal {
+        (, , address token0, address token1, , , , uint128 liquidity, , , , ) =
+            nonfungiblePositionManager.positions(tokenId);
+
+        // set the owner and data for position
+        // operator is msg.sender
+        deposits[tokenId] = Deposit({owner: owner, liquidity: liquidity, token0: token0, token1: token1});
+    }
 
 }
