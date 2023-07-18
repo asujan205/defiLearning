5ccd67b13cdf7574c715f0459f0b169f7ae3d043
asujan205 <asujan205@gmail.com>
2023-07-18 20:25:54 +0545

    Tick

--- contracts/uniswapv3defi/CLAMM/library/Tick.sol
+++ contracts/uniswapv3defi/CLAMM/library/Tick.sol
@@ -2,13 +2,15 @@
 
 // this smart contract is similar to the core contract of uniswap v3 for learning propodrf only 
 pragma solidity ^0.8.19;
+import './TickMath.sol';
 
 
 library Tick {
 
     function tickSpacingToMaxLiquidityPerTick(int24 tickSpacing) internal pure returns (uint128) {
-        // TODO: check this formula 
-        
-        
+        int24 minTick = (TickMath.MIN_TICK / tickSpacing) * tickSpacing;
+        int24 maxTick = (TickMath.MAX_TICK / tickSpacing) * tickSpacing;
+        uint24 numTicks = uint24((maxTick - minTick) / tickSpacing) + 1;
+        return type(uint128).max / numTicks;
     }
 }
