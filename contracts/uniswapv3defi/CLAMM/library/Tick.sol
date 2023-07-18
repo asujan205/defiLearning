b7cf38fab5191457c35dc55093f3f89b949c77d3
asujan205 <asujan205@gmail.com>
2023-07-18 17:32:42 +0545

    #tickSTART

---
+++ contracts/uniswapv3defi/CLAMM/library/Tick.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+// this smart contract is similar to the core contract of uniswap v3 for learning propodrf only 
+pragma solidity ^0.8.19;
+
+
+library Tick {
+
+    function tickSpacingToMaxLiquidityPerTick(int24 tickSpacing) internal pure returns (uint128) {
+        // TODO: check this formula 
+        
+        
+    }
+}
